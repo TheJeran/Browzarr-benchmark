a543a51c2ee077f9b07bc21e9dbe7247ec46d82b
Jeran <thejeran@gmail.com>
2025-04-15 15:02:00 +0200

    Added Perspective to PointCloud

--- src/utils/shaders/pointVertex.glsl
+++ src/utils/shaders/pointVertex.glsl
@@ -6,5 +6,6 @@ uniform bool scalePoints;
 void main() {
     vValue = value/255.;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    gl_PointSize = scalePoints ? pointSize*pow(vValue,2.) : pointSize;
+    float pointScale = pointSize/gl_Position.w;
+    gl_PointSize = scalePoints ? pointScale*pow(vValue,2.) : pointScale ;
 }
