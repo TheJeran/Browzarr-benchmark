7d6736f28eabee45d9b28d496c1784672924641c
Lazaro Alonso <lazarus.alon@gmail.com>
2025-04-16 09:16:23 +0200

    set base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), glsl()],
+  base: '/ViZarrStores/',
   build: {
     target: 'esnext', // support modern features
   },
