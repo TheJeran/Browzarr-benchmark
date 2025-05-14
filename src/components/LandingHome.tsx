2bd36e0105ed204c8bfa013e7f56142cbe205986
Lazaro Alonso <lazarus.alon@gmail.com>
2025-05-14 11:57:09 +0200

    not yet

--- src/components/LandingHome.tsx
+++ src/components/LandingHome.tsx
@@ -7,143 +7,43 @@ import { useState } from 'react';
 
 import { useEffect, useMemo } from 'react';
 import { Analysis, PlotArea, Plot } from '@/components/plots';
-import { GetColorMapTexture, colormaps } from '@/components/textures';
-import { createPaneContainer, MiddleSlider } from '@/components/ui';
+import { GetColorMapTexture } from '@/components/textures';
+import { MiddleSlider } from '@/components/ui';
 import { Metadata, ShowAnalysis, Loading } from '@/components/ui';
-// import ComputeModule from '@/components/computation/ComputeModule'
-import { usePaneInput, usePaneFolder, useTweakpane, useButtonBlade, useTextBlade } from '@lazarusa/react-tweakpane'
 import { useGlobalStore } from '@/utils/GlobalStates';
 import { useShallow } from 'zustand/shallow';
-
-const initStore = GetStore(ZARR_STORES.SEASFIRE)
-
-
-const zarrMetadata = await GetZarrMetadata(ZARR_STORES.SEASFIRE)
-// console.log(zarrMetadata)
-const variables = GetVariableNames(zarrMetadata)
+import { PaneStore } from '@/components/PaneStore';
 
 export function CanvasGeometry() {
+  const { bgcolor, initStore, fullmetadata, variables} = DataStores();
+  const [settings, setSettings] = useState({ variable: 'Default', plotType: 'volume', cmap: 'Spectral', flipCmap: false });
+  const ZarrDS = useMemo(()=>new ZarrDataset(initStore),[initStore])
 
-  const [flipCmap, setFlipCmap] = useState<boolean>(false)
-  
-  const optionsVars = useMemo(() => variables.map((element) => ({
-    text: element,
-    value: element
-  })), []);
-  const colormaps_array = useMemo(() => colormaps.map(colormap => ({
-    text: colormap,
-    value: colormap
-  })), []);
-
-  const optionsStores = useMemo(() => Object.entries(ZARR_STORES).map(([key, value]) => ({
-    text: key,
-    value: value
-  })), []);
-
-  const paneContainer = createPaneContainer("data-settings-pane");
-
-  const pane = useTweakpane(
-    {
-      backgroundcolor: "#292b32",
-      storeURL: ZARR_STORES.SEASFIRE,
-      vName: "Default",
-      plottype: "volume",
-      cmap: "Spectral",
-      flipCmap: false,
-    },
-    {
-      title: 'Data settings',
-      container: paneContainer ?? undefined,
-      expanded: false,
-    }
-  );
-
-  const [bgcolor] = usePaneInput(pane, 'backgroundcolor', {
-    label: 'bgcolor',
-    value: '#292b32'
-  })
-
-  usePaneInput(pane, 'storeURL', {
-    label: 'Store URL',
-    options: optionsStores,
-    value: ZARR_STORES.SEASFIRE
-  })
-// TODO: update variables when custom store is selected
-  const [customStore] = useTextBlade(pane, {
-    label: 'Custom Store',
-    value: 'Setup your own store',
-    parse: (value) => value,
-    format: (value) => value,
-  })
-
-  const [variable] = usePaneInput(pane, 'vName', {
-    label: 'Plot Variable',
-    options: [
-      {
-        text: 'Default',
-        value: 'Default',
-      },
-    ...optionsVars
-  ],
-    value: 'Default'
-  })
-
-  const [plotType] = usePaneInput(pane, 'plottype', {
-    label: 'Plot type',
-    options: [
-      {
-        text: 'volume',
-        value: 'volume',
-      },
-      {
-        text: 'point-cloud',
-        value: 'point-cloud',
-      },
-    ],
-    value: 'point-cloud'
-  })
-
-  const [cmap] = usePaneInput(pane, 'cmap', {
-    label: 'colormap',
-    options: colormaps_array,
-    value: 'Spectral'
-  })
-
-  useButtonBlade(pane,{
-    title:"Flip Colors"
-  },()=>setFlipCmap(x=>!x))
-  
   const setColormap = useGlobalStore(state=>state.setColormap)
   const metadata = useGlobalStore(state=>state.metadata)
   const {colormap, timeSeries} = useGlobalStore(useShallow(state=>({colormap:state.colormap, timeSeries:state.timeSeries})))
   const [showLoading, setShowLoading] = useState<boolean>(false);
 
   //Timeseries Plotting Information
- 
-  const ZarrDS = useMemo(()=>new ZarrDataset(initStore),[])
-
-
   const [canvasWidth, setCanvasWidth] = useState<number>(0)
-
   useEffect(() => {
     setCanvasWidth(Math.round(window.innerWidth * 0.0))
   }, [])
 
   useEffect(()=>{
-    setColormap(GetColorMapTexture(colormap,cmap,1,"#000000",0,flipCmap));
-  },[cmap, flipCmap])
+    setColormap(GetColorMapTexture(colormap, settings.cmap, 1, "#000000", 0, settings.flipCmap));
+  },[settings.cmap,  colormap, settings.flipCmap])
 
   //These values are passed to the Plot Component
   const plotObj = {
-      plotType,
+      plotType: settings.plotType,
       ZarrDS,
-      variable,
+      variable: settings.variable,
       bgcolor,
       canvasWidth
   }
 
 //This is the data being passed down the plot tree
-
   const analysisObj = {
     setters:{
     },
@@ -160,7 +60,7 @@ export function CanvasGeometry() {
       {canvasWidth < 10 && <ShowAnalysis onClick={()=>setCanvasWidth(window.innerWidth*.5)} canvasWidth={canvasWidth} />}
     {canvasWidth > 10 && <MiddleSlider canvasWidth={canvasWidth} setCanvasWidth={setCanvasWidth}/>}
     <Loading showLoading={showLoading} />
-    {canvasWidth > 10 && <Analysis values={analysisObj.values} variables={variables} />}
+    {/* {canvasWidth > 10 && <Analysis values={analysisObj.values} variables={variables} />} */}
     <Plot values={plotObj} setShowLoading={setShowLoading} />
     {metadata && <Metadata data={metadata} /> }
     {timeSeries.length > 2 && <PlotArea />}
