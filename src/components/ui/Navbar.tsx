2d15e5b25a194dbb3c16d078ab82f4bcc3fc9a6e
Lazaro Alonso <lazarus.alon@gmail.com>
2025-05-15 17:17:13 +0200

    favicon

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import { logoSeasFire } from "@/assets/index";
 import { AboutButton } from "@/components/ui";
 import ThemeSwitch  from "@/components/ui/ThemeSwitch";
+import logo from "@/app/logo.png"
 import './css/Navbar.css'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <a>
-        <Image src={logoSeasFire} alt="vizarrstores" />
+      <a href="https://github.com/EarthyScience/Browzarr/" target="_blank" rel="noopener noreferrer">
+        <Image src={logo} alt="browzarr" />
       </a>
       <ThemeSwitch />
       <AboutButton />
