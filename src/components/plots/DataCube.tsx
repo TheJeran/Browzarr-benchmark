af95e0685d854cf5230d8370363d87cccf9019b4
Jeran <thejeran@gmail.com>
2025-06-19 13:22:02 +0200

    Made it so you can adjust the range of the colorbar for high variablility data.

--- src/components/plots/DataCube.tsx
+++ src/components/plots/DataCube.tsx
@@ -16,14 +16,16 @@ export const DataCube = ({ volTexture }: DataCubeProps ) => {
 
     const {shape, colormap, flipY} = useGlobalStore(useShallow(state=>({shape:state.shape, colormap:state.colormap, flipY:state.flipY}))) //We have to useShallow when returning an object instead of a state. I don't fully know the logic yet
 
-    const {valueRange, xRange, yRange, zRange, quality, animate, resetAnim} = usePlotStore(useShallow(state => ({
+    const {valueRange, xRange, yRange, zRange, quality, animate, resetAnim, cScale, cOffset} = usePlotStore(useShallow(state => ({
       valueRange: state.valueRange,
       xRange: state.xRange,
       yRange: state.yRange,
       zRange: state.zRange,
       quality: state.quality,
       animate: state.animate,
-      resetAnim: state.resetAnim
+      resetAnim: state.resetAnim,
+      cScale: state.cScale,
+      cOffset: state.cOffset
     })))
     const [animateProg, setAnimateProg] = useState<number>(0)
   // We need to check if moving this outside of useMemo means it's creating a ton of materials. This was how it was done in THREE Journey when I was doing that, so I know it's not stricly speaking wrong
@@ -32,6 +34,8 @@ export const DataCube = ({ volTexture }: DataCubeProps ) => {
       uniforms: {
           map: { value: volTexture },
           cmap:{value: colormap},
+          cOffset:{value: cOffset},
+          cScale: {value: cScale},
           cameraPos: { value: new THREE.Vector3() },
           threshold: {value: new THREE.Vector2(valueRange[0],valueRange[1])},
           scale: {value: shape},
