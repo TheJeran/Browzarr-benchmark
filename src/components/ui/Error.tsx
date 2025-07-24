955b4153ed3540842acc9524ebaff55c9fdbeab8
Jeran <jpoehls@bgc-jena.mpg.de>
2025-07-24 13:26:08 +0200

    fixed apostrophes and quotations

--- src/components/ui/Error.tsx
+++ src/components/ui/Error.tsx
@@ -46,11 +46,11 @@ const Error = () => {
                 {cors &&
                 <div>
                     This dataset is hosted on a different website and your browser is blocking it for security reasons. 
-                    It's called a "CORS" issue (Cross-Origin Resource Sharing). 
+                    It&apos;s called a &quot;CORS&quot; issue (Cross-Origin Resource Sharing). 
                     The server you're trying to access hasn't given permission for browsers to fetch data from other websites like this one. <br/><br/>
                     If you access it through code outside a browser (i.e., in Python), it works fine - because those environments 
-                    don’t follow browser security rules. <br/><br/>
-                    To fix this, the dataset’s hosting server needs to allow access by adding headers that tell your browser “this is safe.” 
+                    don&apos;t follow browser security rules. <br/><br/>
+                    To fix this, the dataset&apos;s hosting server needs to allow access by adding headers that tell your browser &quot;this is safe.&quot; 
                     If you control the server, you can add those CORS headers. Otherwise, reach out to whoever manages the data and ask them to enable CORS for browser access.
 
                 </div>
@@ -59,7 +59,7 @@ const Error = () => {
                 {oom &&
                 <div>
                     Whoa there! <br/><br/>
-                    If you're reading this you've just tried to plot a ridiculous amount of points. Your browser ran out of memory allocating the arrays 
+                    If you&apos;re reading this you&apos;ve just tried to plot a ridiculous amount of points. Your browser ran out of memory allocating the arrays 
                     necessary to dictate their positions in space. Stick to the volumetric view if you need to view the entire dataset.
                 </div>
                 }
