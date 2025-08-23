e69c3d02387c64dce462feae9be18770533ce4ab
Lazaro Alonso <lazarus.alon@gmail.com>
2025-08-23 17:45:29 +0200

    do root (#231)

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export' as const,
-  basePath: process.env.NODE_ENV === 'production' ? '/Browzarr' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
   images: {
     unoptimized: true,
   },
